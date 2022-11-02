2d9525cf29e34e92c24c3655c15c175db18384eb
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-11-02 12:30:50 +0200

    add pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from 'pages/Home';
+import Movies from 'pages/Movies';
+import NotFound from 'pages/NotFound';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">Movies</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
