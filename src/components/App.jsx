c5068a0f8db1c1e11728aac79d5135dd2db29f78
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-11-02 17:12:25 +0200

    add route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,8 @@ import { Routes, Route, Link } from 'react-router-dom';
 import { Home } from 'pages/Home';
 import { Movies } from 'pages/Movies';
 import { NotFound } from 'pages/NotFound';
-import { Details } from 'pages/Details';
-import { Credits } from 'pages/Credits';
+import { MovieDetails } from 'pages/MovieDetails';
+import { Cast } from 'pages/Cast';
 import { Reviews } from 'pages/Reviews';
 
 export const App = () => {
@@ -16,9 +16,9 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:detailsId" element={<Details />} />
-        <Route path="/movies/:creditsId" element={<Credits />} />
-        <Route path="/movies/:reviewsId" element={<Reviews />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />} />
+        <Route path="/movies/:movieId/cast" element={<Cast />} />
+        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
--- src/pages/Details.jsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-export const Details = () => {
-  const { detailsId } = useParams();
-  return <div>Now showing product with id - {detailsId}</div>;
-};
-  
