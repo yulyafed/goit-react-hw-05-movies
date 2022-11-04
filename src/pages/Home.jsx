c22dbcfc556c494a801ef8c1ca670ee5a452a0c0
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-11-04 15:56:09 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
 import { fetchTrendMovies } from "ApiService";
 
 export const Home = () => {
-  // return fetchTrendMovies();
+
+fetchTrendMovies().then((response) => {
+    return response.data.results;
+  });
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="">
+            
+          </a>
+       </li>
+    
+      </ul>
+  </div>
+)
 };
