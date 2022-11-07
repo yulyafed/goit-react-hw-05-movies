e0a6e53cfd9a612fbf6449198e4675d23f870f90
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-11-07 15:42:20 +0200

    update cast

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -11,6 +11,7 @@ export const Cast = ({ id }) => {
     async function updateMovieCasts(id) {
       const response = await fetchMovieCredits(id);
       setMovieCasts(response.data);
+      console.log(response.data);
       
     }
     updateMovieCasts(id);
@@ -23,7 +24,9 @@ export const Cast = ({ id }) => {
           {moviecasts.cast.map(item => {
             return (
               <li key={item.id}>
-                <img src={posterSmallUrlPrefix + item.profile_path} alt="" />
+                {item.profile_path !== null && (
+                  <img src={posterSmallUrlPrefix + item.profile_path} alt="" />
+                )}
                 <p> {item.original_name} </p>
                 <p>Character:{item.character}</p>
               </li>
