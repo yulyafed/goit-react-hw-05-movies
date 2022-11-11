c12a3dd7de871e3d892a3b50fccb93302cceebd9
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-11-11 16:15:55 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
-// import { lazy, Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 import { Layout } from 'components/Layout';
 import { NotFound } from 'components/NotFound';
-import { Home } from 'pages/Home';
-import { Movies } from 'pages/Movies';
-import { MovieDetails } from 'pages/MovieDetails';
-import { Cast } from 'components/Cast';
-import { Reviews } from 'components//Reviews';
+// import { Home } from 'pages/Home';
+// import { Movies } from 'pages/Movies';
+// import { MovieDetails } from 'pages/MovieDetails';
+// import { Cast } from 'components/Cast';
+// import { Reviews } from 'components//Reviews';
 
-// const createAsyncComponent = (path) => lazy(() => import(path));
+const createAsyncComponent = (path) => lazy(() => import(path));
 
-// const Home = createAsyncComponent('../pages/Home');
-// const Movies = createAsyncComponent('../pages/Movies');
-// const MovieDetails = createAsyncComponent('../pages/MovieDetails');
-// const Cast = createAsyncComponent('./Cast');
-// const Reviews = createAsyncComponent('./Reviews');
+const Home = createAsyncComponent('../pages/Home');
+const Movies = createAsyncComponent('../pages/Movies');
+const MovieDetails = createAsyncComponent('../pages/MovieDetails');
+const Cast = createAsyncComponent('./Cast');
+const Reviews = createAsyncComponent('./Reviews');
 
 export const App = () => {
   return (
 
-    // <Suspense fallback={<div>Loading</div>}>
+    <Suspense fallback={<div>Loading</div>}>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
@@ -31,6 +31,6 @@ export const App = () => {
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
-    // </Suspense>
+     </Suspense>
   );
 };
